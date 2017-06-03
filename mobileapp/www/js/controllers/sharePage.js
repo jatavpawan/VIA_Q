e8a111e9a4e7159d861eec403afb5492a893bc8f
jatav.pawan <pawan.j@cisinlabs.com>
2017-06-03 20:21:03 +0530

    implmented change on creating ticket section for footer

--- mobileapp/www/js/controllers/sharePage.js
+++ mobileapp/www/js/controllers/sharePage.js
@@ -4,17 +4,17 @@
     // $scope.reviewStar = 4;
 
     $scope.ShareTwitter = function () {
-        alert('this is twitter');
+        //alert('this is twitter');
         window.plugins.socialsharing.shareViaTwitter('Message via Twitter');
         //window.plugins.socialsharing.shareViaTwitter('Message and link via Twitter', null /* img */, 'http://www.x-services.nl');
     }
     $scope.ShareFacebook = function () {
-        alert('this is FaceBook');
+       // alert('this is FaceBook');
         window.plugins.socialsharing.shareViaFacebook('Message via Facebook', null /* img */, null /* url */, function () { console.log('share ok') }, function (errormsg) { alert(errormsg) })
     }
 
     $scope.ShareInstagram = function () {
-        alert('This is Instagram');
+       // alert('This is Instagram');
         window.plugins.socialsharing.shareViaInstagram('Message via Instagram', 'https://www.google.nl/images/srpr/logo4w.png', function () { console.log('share ok') }, function (errormsg) { alert(errormsg) });
     }
 
