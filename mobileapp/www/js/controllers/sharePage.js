aec148b25899aacc1f7315c64550de006d5433ef
sachin patidar <sachin.patidar4@gmail.com>
2017-06-09 14:42:39 +0530

    added sharing

--- mobileapp/www/js/controllers/sharePage.js
+++ mobileapp/www/js/controllers/sharePage.js
@@ -1,23 +1,109 @@
 ﻿angular.module('sharePage.controllers', [])
 
-.controller('sharePageCtrl', function ($scope, $state) {
+.controller('sharePageCtrl', function ($scope, $state, $ionicModal, $timeout, $cordovaInAppBrowser) {
     // $scope.reviewStar = 4;
+    var options = {
+        location: 'yes',
+        clearcache: 'yes',
 
+        closebuttoncaption: 'Close'
+    };
+    var url;
+    $ionicModal.fromTemplateUrl('templates/goToStore.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.modalRes = modal;
+
+    });
+    $scope.closeModal = function () {
+        $scope.modalRes.hide();
+    }
+
+    $scope.message = "La aplicación no está instalada en tu dispositivo. ¿Deseas instalarlo?";
     $scope.ShareTwitter = function () {
         //alert('this is twitter');
-        window.plugins.socialsharing.shareViaTwitter('Message via Twitter');
+     
+        window.plugins.socialsharing.shareViaTwitter('Message via Twitter', '', '', function (suc) { }, function (er) {
+            $timeout(function () {
+                $scope.modalRes.show();
+            }, 0)
+            if (ionic.Platform.isIOS() || ionic.Platform.isIPad()) {
+               url='http://www.google.com'
+            }
+            else {
+               url='http://www.google.com'
+            }
+        });
         //window.plugins.socialsharing.shareViaTwitter('Message and link via Twitter', null /* img */, 'http://www.x-services.nl');
     }
     $scope.ShareFacebook = function () {
        // alert('this is FaceBook');
-        window.plugins.socialsharing.shareViaFacebook('Message via Facebook', null /* img */, null /* url */, function () { console.log('share ok') }, function (errormsg) { alert(errormsg) })
+        window.plugins.socialsharing.shareViaFacebook('Message via Facebook', null /* img */, null /* url */, function () { console.log('share ok') }, function (errormsg) {
+            $timeout(function () {
+                $scope.modalRes.show();
+            }, 0);
+            if (ionic.Platform.isIOS() || ionic.Platform.isIPad()) {
+               url='http://www.google.com'
+            }
+            else {
+                url='http://www.google.com'
+            }
+        })
     }
 
     $scope.ShareInstagram = function () {
        // alert('This is Instagram');
-        window.plugins.socialsharing.shareViaInstagram('Message via Instagram', 'https://www.google.nl/images/srpr/logo4w.png', function () { console.log('share ok') }, function (errormsg) { alert(errormsg) });
+        window.plugins.socialsharing.shareViaInstagram('Message via Instagram', 'https://www.google.nl/images/srpr/logo4w.png', '', '', function () { console.log('share ok') }, function (errormsg) {
+            $scope.modalRes.show();
+            if (ionic.Platform.isIOS() || ionic.Platform.isIPad()) {
+                url='http://www.google.com'
+            }
+            else {
+                url='http://www.google.com'
+            }
+        });
     }
+$scope.ShareLinkedIn=function(){
+    if (ionic.Platform.isIOS() || ionic.Platform.isIPad()) {
+        $scope.shareVia('', 'https://www.google.nl/images/srpr/logo4w.png');
+        url='http://www.google.com'
+    }
+    else {
+        url='http://www.google.com'
+        $scope.shareVia('com.linkedin.android', 'https://www.google.nl/images/srpr/logo4w.png')
+    }
+}
+
+$scope.ShareGooglePlus=function(){
+    if (ionic.Platform.isIOS() || ionic.Platform.isIPad()) {
+        $scope.shareVia('', 'https://www.google.nl/images/srpr/logo4w.png')
+        url='http://www.google.com'
+    }
+    else {
+        url='http://www.google.com'
+        $scope.shareVia('com.google.android.apps.plus', 'https://www.google.nl/images/srpr/logo4w.png')
+    }
+}
+$scope.shareVia = function (packageName,text) {
+    window.plugins.socialsharing.shareVia(packageName, text, '', '', '', function () { console.log('share ok') }, function (msg) {
+        $scope.modalRes.show();
+    })
+}
+$scope.goToStore = function () {
+   
+    document.addEventListener("deviceready", function () {
+       
+        $cordovaInAppBrowser.open(url, '_system', options)
+          .then(function (event) {
+              // success
+          })
+          .catch(function (event) {
+              // error
+          });
 
 
+        // $cordovaInAppBrowser.close();
 
+    }, false);
+}
 })
